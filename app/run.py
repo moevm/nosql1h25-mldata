bcb6679a699d685254f01523f7bdbde3fa3c6ead
Sergey Sokolov <sokolovserej@gmail.com>
2025-04-14 21:28:11 +0300

    [UPD] configure mongo

--- app/run.py
+++ app/run.py
@@ -2,14 +2,23 @@
 Создает сущность Flask и запускает ее. Добавляет к ней пути с помощью blueprint.
 """
 
+import os
+from urllib.parse import quote_plus
+
 from flask import Flask
 
-from app.src.routers import dataset_routes
+from src.routers import dataset_routes
 
 if __name__ == '__main__':
     app: Flask = Flask(__name__)
     app.register_blueprint(dataset_routes.bp)
 
-    print('http://127.0.0.1:5000/datasets')
+    user = os.getenv("USR")
+    password = os.getenv("PWD")
+    host = os.getenv("HOST")
+    port = os.getenv("PORT")
+
+    app.config['MONGO_URI'] = f"mongodb://{quote_plus(user)}:{quote_plus(password)}@{host}:{port}"
+    app.config['UPLOAD_FOLDER'] = os.getenv('DATASET_DIR', './datasets')
 
     app.run()
