16a0aedede85fe5b648e8f91baeb0606cb2c5f9d
Sergey Sokolov <sokolovserej@gmail.com>
2025-04-14 22:27:07 +0300

    [UPD] get dataset in routes

--- app/src/routers/dataset_routes.py
+++ app/src/routers/dataset_routes.py
@@ -1,7 +1,10 @@
 """
 Настраивает пути для endpoint'ов приложения. Сохраняет их в blueprint.
 """
-from flask import Blueprint
+from typing import Union
+
+from flask import Blueprint, request
+from werkzeug.exceptions import BadRequest
 
 from app.src.controllers.dataset_controller import DatasetController
 
@@ -14,3 +17,18 @@ def get_datasets() -> str:
     Обращается с методу контроллера для отображения страницы со всеми датасетами.
     """
     return DatasetController.render_all_datasets()
+
+
+@bp.route('/dataset/<dataset_id>/', methods=['GET'])
+def get_dataset(dataset_id: str) -> Union[str, BadRequest]:
+    """
+    Обращается к методам контроллера:
+        GET - получения датасета из БД
+    """
+    if request.method != 'GET':
+        return BadRequest('Invalid method')
+
+    try:
+        return DatasetController.get_dataset(dataset_id)
+    except Exception:
+        return BadRequest('Item can\'t be found')
