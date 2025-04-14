6435d092eb2bf54ff1f7ebe602bead7ffde0572a
Sergey Sokolov <sokolovserej@gmail.com>
2025-04-14 22:47:15 +0300

    [UPD] update method in service

--- app/src/services/dataset_service.py
+++ app/src/services/dataset_service.py
@@ -30,5 +30,16 @@ class DatasetService:
         return DatasetRepository.add_dataset(dataset_csv)
 
     @staticmethod
+    def update_dataset(dataset_id: int, form_values: DatasetFormValues, author: str, filepath: str) -> None:
+        """
+        Создает объект Dataset на основе `form_values`.
+        Обращается к методу репозитория для изменения датасета в БД.
+        """
+        dataset: Dataset = Dataset.from_form_values(form_values, author, filepath)
+        dataset.dataset_id = dataset_id
+
+        DatasetRepository.edit_dataset(dataset)
+
+    @staticmethod
     def get_dataset(dataset_id: str) -> Dataset:
         return DatasetRepository.get_dataset(dataset_id)
