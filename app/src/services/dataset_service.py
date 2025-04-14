3bbcec67c1c7154d699b0bffb6df30762a2f5eb3
Sergey Sokolov <sokolovserej@gmail.com>
2025-04-14 22:42:47 +0300

    [UPD] save dataset method as service

--- app/src/services/dataset_service.py
+++ app/src/services/dataset_service.py
@@ -1,7 +1,10 @@
 """
 Содержит сервисы приложения. Сервис представляет бизнес-логику приложения.
 """
+from bson import ObjectId
+
 from app.src.models.Dataset import Dataset
+from app.src.models.DatasetFormValues import DatasetFormValues
 from app.src.repository.dataset_repository import DatasetRepository
 
 
@@ -16,6 +19,15 @@ class DatasetService:
         Обращается к методу репозитория для получения списка Brief'ов всех датасетов в БД.
         """
         return DatasetRepository.get_all_datasets_brief()
+
+    @staticmethod
+    def save_dataset(form_values: DatasetFormValues, author: str, filepath: str) -> ObjectId:
+        """
+        Создает объект Dataset на основе `form_values`.
+        Обращается к методу репозитория для добавления датасета в БД.
+        """
+        dataset_csv: Dataset = Dataset.from_form_values(form_values, author, filepath)
+        return DatasetRepository.add_dataset(dataset_csv)
 
     @staticmethod
     def get_dataset(dataset_id: str) -> Dataset:
