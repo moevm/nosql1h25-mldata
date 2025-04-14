300552c0223e988be0b02f152296abc9520e4be1
Sergey Sokolov <sokolovserej@gmail.com>
2025-04-14 21:42:39 +0300

    [UPD] add and edit datasets in database

--- app/src/repository/dataset_repository.py
+++ app/src/repository/dataset_repository.py
@@ -2,12 +2,16 @@
 Содержит репозитории приложения.
 Репозиторий напрямую работает с БД - добавляет, изменяет, удаляет и ищет записи в БД.
 """
+import json
 import os
+from typing import Optional
 
+from bson import ObjectId
 from flask import current_app, g
 from flask_pymongo import PyMongo
 from werkzeug.local import LocalProxy
 
+from app.src.models.Dataset import Dataset
 from app.src.models.DatasetBrief import DatasetBrief
 
 
@@ -49,3 +53,17 @@ class DatasetRepository:
         ]
 
         return briefs
+
+    @staticmethod
+    def add_dataset(dataset: Dataset) -> None:
+        """
+        Добавляет датасет в БД.
+        """
+        db['DatasetCollection'].insert_one(dataset.to_dict())
+
+    @staticmethod
+    def edit_dataset(dataset: Dataset) -> None:
+        """
+        Изменяет датасет в БД.
+        """
+        db['DatasetCollection'].update_one(dataset.to_dict())
