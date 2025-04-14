4575291596b605b27e929206418e21a1c83b6ab1
Sergey Sokolov <sokolovserej@gmail.com>
2025-04-14 22:24:06 +0300

    [UPD] get dataset in service

--- app/src/services/dataset_service.py
+++ app/src/services/dataset_service.py
@@ -1,7 +1,7 @@
 """
 Содержит сервисы приложения. Сервис представляет бизнес-логику приложения.
 """
-
+from app.src.models.Dataset import Dataset
 from app.src.repository.dataset_repository import DatasetRepository
 
 
@@ -16,3 +16,7 @@ class DatasetService:
         Обращается к методу репозитория для получения списка Brief'ов всех датасетов в БД.
         """
         return DatasetRepository.get_all_datasets_brief()
+
+    @staticmethod
+    def get_dataset(dataset_id: str) -> Dataset:
+        return DatasetRepository.get_dataset(dataset_id)
