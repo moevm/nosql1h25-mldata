6e6da8c5ceecb868e5822cfba0cde96defcbeb9d
Sergey Sokolov <sokolovserej@gmail.com>
2025-04-14 22:44:26 +0300

    [UPD] add dataset method in controller for write dataset as file and info in bd

--- app/src/controllers/dataset_controller.py
+++ app/src/controllers/dataset_controller.py
@@ -2,8 +2,9 @@
 Содержит контроллеры приложения. Контроллер является связным звеном между запросами с клиента и бизнес-логикой.
 """
 import json
+import os
 
-from flask import jsonify
+from flask import Request, Response, current_app, make_response, jsonify
 from werkzeug.datastructures import FileStorage
 
 from app.src.models.Dataset import Dataset
@@ -26,6 +27,30 @@ class DatasetController:
         return json.dumps([brief.to_dict() for brief in all_datasets_brief])
 
     @staticmethod
+    def add_dataset(request: Request) -> Response:
+        """
+        Создается объект DatasetFormData из данных request'а.
+        Обращается к методу сервиса для добавления датасета в БД.
+        Сохраняется файл в выделенной директории.
+        Возвращается response, содержащий URL страницы, открываемой после добавления.
+        """
+        username = 'username'
+
+        form_values: DatasetFormValues = DatasetController._extract_form_values(request)
+
+        filepath: str = current_app.config['UPLOAD_FOLDER']
+        dataset_id = DatasetService.save_dataset(form_values, author=username, filepath=filepath)
+
+        filepath = os.path.join(filepath, f'{dataset_id}.csv')
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)
+        with open(filepath, 'x') as file:
+            file.writelines(form_values.dataset_data)
+
+        response: Response = make_response()
+        response.headers['redirect'] = '/datasets/'
+        return response
+
+    @staticmethod
     def get_dataset(dataset_id: str):
         """
         Возвращается json, содержащий данные о датасете.
@@ -40,5 +65,5 @@ class DatasetController:
         dataset_description: str = form_data['description']
 
         dataset_fs: FileStorage = request.files['dataset']
-        dataset_data = ''.join([v.decode('utf-8') for v in dataset_fs.readlines()])
+        dataset_data = '\n'.join([v.decode('utf-8').strip() for v in dataset_fs.readlines()])
         return DatasetFormValues(dataset_name, dataset_description, dataset_data)
