aa35ebb94a93fcd920c7fdd5571304b82e69915c
jqnfxa <ivborisov@stud.etu.ru>
2025-04-15 23:19:15 +0300

    [UPD]: app run

--- app/run.py
+++ app/run.py
@@ -1,15 +1,58 @@
 """
 Создает сущность Flask и запускает ее. Добавляет к ней пути с помощью blueprint.
+Настраивает Flask-Login.
 """
 
-from flask import Flask
+import os
+from flask import Flask, redirect, url_for, flash
+from flask_login import LoginManager, current_user
 
 from app.src.routers import dataset_routes
+from app.src.routers import auth_routes
+from app.src.repository.user_repository import UserRepository
+
+# --- Flask App Initialization ---
+app: Flask = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY', 'secret_key')
+
+# --- Flask-Login Initialization ---
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'auth.login'
+login_manager.login_message = "Please log in to access this page."
+login_manager.login_message_category = "info"
+
+@login_manager.user_loader
+def load_user(user_id: str):
+    """
+    Callback function used by Flask-Login to reload the user object from the user ID stored in the session.
+    """
+    return UserRepository.find_by_id(user_id)
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    """
+    Handles unauthorized access attempts. Redirects to login page.
+    """
+    flash("You need to be logged in to view this page.", "warning")
+    return redirect(url_for('auth.login', next=request.path))
+
+
+app.register_blueprint(dataset_routes.bp)
+app.register_blueprint(auth_routes.auth_bp)
+
+
+@app.route('/')
+def index():
+    if current_user.is_authenticated:
+        return redirect(url_for('datasets.get_datasets'))
+    else:
+        return redirect(url_for('auth.login'))
+
 
 if __name__ == '__main__':
-    app: Flask = Flask(__name__)
-    app.register_blueprint(dataset_routes.bp)
+    print('App starting...')
+    print('Login available at: http://127.0.0.1:5000/login')
+    print('Datasets available at: http://127.0.0.1:5000/datasets (requires login)')
 
-    print('http://127.0.0.1:5000/datasets')
-
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
