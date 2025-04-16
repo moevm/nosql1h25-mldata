40ec9864d04b3b1a77419120a8b91e40dbb322b4
jqnfxa <ivborisov@stud.etu.ru>
2025-04-16 22:29:30 +0300

    [REF]: move to routes

--- app/run.py
+++ app/run.py
@@ -5,66 +5,18 @@
 
 import os
 from flask import Flask, redirect, url_for, flash, request
-from flask_login import LoginManager, current_user
 
-try:
-    from src.routers import dataset_routes
-    from src.routers import auth_routes
-    from src.repository.user_repository import UserRepository
-except ImportError:
-    from app.src.routers import dataset_routes
-    from app.src.routers import auth_routes
-    from app.src.repository.user_repository import UserRepository
-
+from app.src.routers import dataset_routes
+from app.src.routers import auth_routes
 
 # --- Flask App Initialization ---
-app: Flask = Flask(__name__)
+app: Flask = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY', 'default_secret_key_change_me')
 
-# --- Flask-Login Initialization ---
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-login_manager.login_view = 'auth.login'
-login_manager.login_message = "Log in to access this page."
-login_manager.login_message_category = "warning"
-
-@login_manager.user_loader
-def load_user(user_id: str):
-    """
-    Функция, используемая Flask-Login для перезагрузки 
-    пользовательского объекта из идентификатора пользователя, сохраненного в сеансе.
-    """
-    return UserRepository.find_by_id(user_id)
-
-@login_manager.unauthorized_handler
-def unauthorized():
-    """
-    Обработка несанкционированного доступа. 
-    Перенаправляет на страницу входа в систему с сохранением первоначально запрошенного URL.
-    """
-    flash("Для доступа к странице требуется авторизация", "warning")
-    next_url = request.path
-    if request.query_string:
-        next_url += '?' + request.query_string.decode('utf-8')
-
-    return redirect(url_for('auth.login', next=next_url))
-
-
 # --- Register Blueprints ---
+auth_routes.login_manager.init_app(app)
 app.register_blueprint(dataset_routes.bp)
 app.register_blueprint(auth_routes.auth_bp)
-
-
-# --- Root Route ---
-@app.route('/')
-def index():
-    """Переход к страницам в зависимости от состояния авторизации"""
-    if current_user.is_authenticated:
-        return redirect(url_for('datasets.get_datasets'))
-    else:
-        return redirect(url_for('auth.login'))
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=os.environ.get('FLASK_DEBUG', 'True').lower() == 'true')
--- app/src/routers/auth_routes.py
+++ app/src/routers/auth_routes.py
@@ -1,13 +1,20 @@
 """
-Настраивает пути для endpoint'ов аутентификации (логин, логаут).
+Настраивает пути для endpoint'ов аутентификации (login/logout).
 """
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
+from flask_login import LoginManager
 from urllib.parse import urlparse
 
 from app.src.repository.user_repository import UserRepository
 
 auth_bp: Blueprint = Blueprint('auth', __name__, template_folder='templates')
+
+# --- Flask-Login Initialization ---
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.login_message = "Для доступа к странице требуется авторизация"
+login_manager.login_message_category = "warning"
 
 # --- Routes ---
 @auth_bp.route('/login', methods=['GET', 'POST'])
@@ -57,3 +64,32 @@ def logout():
     logout_user()
     flash('Вы вышли из системы', 'info')
     return redirect(url_for('auth.login'))
+
+@login_manager.user_loader
+def load_user(user_id: str):
+    """
+    Функция, используемая Flask-Login для перезагрузки 
+    пользовательского объекта из идентификатора пользователя, сохраненного в сеансе.
+    """
+    return UserRepository.find_by_id(user_id)
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    """
+    Обработка несанкционированного доступа. 
+    Перенаправляет на страницу входа в систему с сохранением первоначально запрошенного URL.
+    """
+    flash("Для доступа к странице требуется авторизация", "warning")
+    next_url = request.path
+    if request.query_string:
+        next_url += '?' + request.query_string.decode('utf-8')
+
+    return redirect(url_for('auth.login', next=next_url))
+
+@auth_bp.route('/')
+def index():
+    """Переход к страницам в зависимости от состояния авторизации"""
+    if current_user.is_authenticated:
+        return redirect(url_for('datasets.get_datasets'))
+    else:
+        return redirect(url_for('auth.login'))
