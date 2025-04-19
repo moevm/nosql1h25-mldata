ad1e32b2a2b77b1f5aa2c4c1ac1ee9455ae18104
Kirill Karpunin <kirill.karpunin20014@gmail.com>
2025-04-19 16:22:55 +0300

    [ADD] Create filter by data size

--- app/static/scripts/filter.js
+++ app/static/scripts/filter.js
@@ -1,21 +1,43 @@
-function setupSearchFilter() {
-    const $allCards = $('.cards-container .card');
+function setupFilters() {
+    const $cardsContainer = $('.cards-container');
+    let $allCards = $cardsContainer.children('.card').detach();
 
-    function applyFilters() {
+    function applyFiltersAndSort() {
         const searchText = $('#name-filter').val().toLowerCase();
 
-        $allCards.each(function() {
-            const $card = $(this);
-            const cardName = $card.data('name').toLowerCase();
-            const isVisible = cardName.includes(searchText);
+        const sizeFrom = parseInt($('#data-size-from').val()) || 0;
+        const sizeTo = parseInt($('#data-size-to').val()) || Infinity;
 
-            $card.toggle(isVisible);
+        const sortOrder = $('#data-size-sort').val();
+
+        const filteredCards = $allCards.filter(function() {
+            const $card = $(this);
+
+            const cardName = $card.data('name').toLowerCase();
+            const cardSize = parseInt($card.data('size')) || 0;
+
+            const nameMatch = searchText === '' || cardName.includes(searchText);
+            const sizeMatch = cardSize >= sizeFrom && cardSize <= sizeTo;
+
+            return nameMatch && sizeMatch;
         });
+
+        const sortedCards = filteredCards.sort(function(a, b) {
+            const sizeA = parseInt($(a).data('size')) || 0;
+            const sizeB = parseInt($(b).data('size')) || 0;
+
+            return sortOrder === 'asc' ? sizeA - sizeB : sizeB - sizeA;
+        });
+
+        $cardsContainer.empty().append(sortedCards);
     }
 
-    $('#name-filter').on('input', applyFilters);
+    $('#name-filter, #data-size-from, #data-size-to').on('input change', applyFiltersAndSort);
+    $('#data-size-sort').on('change', applyFiltersAndSort);
+
+    applyFiltersAndSort();
 }
 
 $(document).ready(function() {
-    setupSearchFilter();
+    setupFilters();
 });
