eb5d5f9a54464ca43441bc2d971a540987903922
Kirill Karpunin <kirill.karpunin20014@gmail.com>
2025-04-20 15:08:19 +0300

    [REF] Rework multiple sortings system

--- app/static/scripts/filter.js
+++ app/static/scripts/filter.js
@@ -1,75 +1,86 @@
-function setupFilters() {
-    const $cardsContainer = $('.cards-container');
-    let $allCards = $cardsContainer.children('.card').detach();
+let $allCards;
+let $cardsContainer;
+let currentSort = null;
 
-    function applyFiltersAndSort() {
-        const searchText = $('#name-filter').val().toLowerCase();
+function applyFilters() {
+    const searchText = $('#name-filter').val().toLowerCase();
 
-        let sizeFrom = parseInt($('#data-size-from').val()) || 0;
-        let sizeTo = parseInt($('#data-size-to').val()) || Infinity;
-        if (sizeTo < sizeFrom) sizeTo = Infinity;
+    let sizeFrom = parseInt($('#data-size-from').val()) || 0;
+    let sizeTo = parseInt($('#data-size-to').val()) || Infinity;
+    if (sizeTo < sizeFrom) sizeTo = Infinity;
 
-        let rowSizeFrom = parseInt($('#row-size-from').val()) || 0;
-        let rowSizeTo = parseInt($('#row-size-to').val()) || Infinity;
-        if (rowSizeTo < rowSizeFrom) rowSizeTo = Infinity;
+    let rowSizeFrom = parseInt($('#row-size-from').val()) || 0;
+    let rowSizeTo = parseInt($('#row-size-to').val()) || Infinity;
+    if (rowSizeTo < rowSizeFrom) rowSizeTo = Infinity;
 
-        let viewsFrom = parseInt($('#views-from').val()) || 0;
-        let viewsTo = parseInt($('#views-to').val()) || Infinity;
-        if (viewsTo < viewsFrom) viewsTo = Infinity;
+    let viewsFrom = parseInt($('#views-from').val()) || 0;
+    let viewsTo = parseInt($('#views-to').val()) || Infinity;
+    if (viewsTo < viewsFrom) viewsTo = Infinity;
 
-        let downloadsFrom = parseInt($('#downloads-from').val()) || 0;
-        let downloadsTo = parseInt($('#downloads-to').val()) || Infinity;
-        if (downloadsTo < downloadsFrom) downloadsTo = Infinity;
+    let downloadsFrom = parseInt($('#downloads-from').val()) || 0;
+    let downloadsTo = parseInt($('#downloads-to').val()) || Infinity;
+    if (downloadsTo < downloadsFrom) downloadsTo = Infinity;
 
-        const filteredCards = $allCards.filter(function() {
-            const $card = $(this);
+    return $allCards.filter(function () {
+        const $card = $(this);
+        const cardName = $card.data('name').toLowerCase();
+        const cardSize = parseInt($card.data('size')) || 0;
+        const cardRowSize = parseInt($card.data('row-size')) || 0;
+        const cardViews = parseInt($card.data('views')) || 0;
+        const cardDownloads = parseInt($card.data('downloads')) || 0;
 
-            const cardName = $card.data('name').toLowerCase();
-            const cardSize = parseInt($card.data('size')) || 0;
-            const cardRowSize = parseInt($card.data('row-size')) || 0;
-            const cardViews = parseInt($card.data('views')) || 0;
-            const cardDownloads = parseInt($card.data('downloads')) || 0;
-
-            const nameMatch = searchText === '' || cardName.includes(searchText);
-            const sizeMatch = cardSize >= sizeFrom && cardSize <= sizeTo;
-            const rowSizeMatch = cardRowSize >= rowSizeFrom && cardRowSize <= rowSizeTo;
-            const viewsMatch = cardViews >= viewsFrom && cardViews <= viewsTo;
-            const downloadsMatch = cardDownloads >= downloadsFrom && cardDownloads <= downloadsTo;
-
-            return nameMatch && sizeMatch && rowSizeMatch && viewsMatch && downloadsMatch;
-        });
-
-        const sortPriority = [
-            $('#data-size-sort').val(),
-            $('#row-size-sort').val(),
-            $('#downloads-sort').val(),
-            $('#views-sort').val()
-        ].filter(Boolean);
-
-        const sortedCards = filteredCards.sort((a, b) => {
-            for (const sortType of sortPriority) {
-                const [field, order] = sortType.split('_');
-                const valueA = $(a).data(field);
-                const valueB = $(b).data(field);
-
-                if (valueA !== valueB) {
-                    return order === 'asc' ?
-                        (valueA - valueB) :
-                        (valueB - valueA);
-                }
-            }
-            return 0;
-        });
-
-        $cardsContainer.empty().append(sortedCards);
-    }
-
-    $('#name-filter, #data-size-from, #data-size-to, #row-size-from, #row-size-to, #views-from, #views-to, #downloads-from, #downloads-to').on('input change', applyFiltersAndSort);
-    $('#data-size-sort, #row-size-sort, #views-sort, #downloads-sort').on('change', applyFiltersAndSort);
-
-    applyFiltersAndSort();
+        return (searchText === '' || cardName.includes(searchText)) &&
+            (cardSize >= sizeFrom && cardSize <= sizeTo) &&
+            (cardRowSize >= rowSizeFrom && cardRowSize <= rowSizeTo) &&
+            (cardViews >= viewsFrom && cardViews <= viewsTo) &&
+            (cardDownloads >= downloadsFrom && cardDownloads <= downloadsTo);
+    });
 }
 
-$(document).ready(function() {
+function applySort(cards) {
+    if (!currentSort) return cards;
+
+    return cards.toArray().sort((a, b) => {
+        const valueA = $(a).data(currentSort.field);
+        const valueB = $(b).data(currentSort.field);
+        return currentSort.order === 'asc' ? valueA - valueB : valueB - valueA;
+    });
+}
+
+function updateDisplay() {
+    const filtered = applyFilters();
+    const sorted = applySort(filtered);
+    $cardsContainer.empty().append(sorted);
+}
+
+function setupFilters() {
+    $cardsContainer = $('.cards-container');
+    $allCards = $cardsContainer.children('.card').detach();
+
+    $('#name-filter, #data-size-from, #data-size-to, #row-size-from, #row-size-to, #views-from, #views-to, #downloads-from, #downloads-to')
+        .on('input change', updateDisplay);
+
+    updateDisplay();
+}
+
+function setupSorting() {
+    $('#data-size-sort, #row-size-sort, #views-sort, #downloads-sort').on('change', function () {
+        const val = $(this).val();
+        currentSort = val ? {
+            field: val.split('_')[0],
+            order: val.split('_')[1]
+        } : null;
+
+        if (currentSort) resetOtherSorts(this);
+        updateDisplay();
+    });
+}
+
+function resetOtherSorts(currentElement) {
+    $('#data-size-sort, #row-size-sort, #views-sort, #downloads-sort').not(currentElement).val('');
+}
+
+$(document).ready(function () {
     setupFilters();
+    setupSorting();
 });
