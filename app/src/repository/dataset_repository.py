437a6cf87e1134ca3d056fa22828a768c56864d5
Sergey Sokolov <sokolovserej@gmail.com>
2025-04-20 21:36:58 +0300

    [UPD] add repository delete func

--- app/src/repository/dataset_repository.py
+++ app/src/repository/dataset_repository.py
@@ -16,7 +16,6 @@ from werkzeug.local import LocalProxy
 from app.src.models.Dataset import Dataset
 from app.src.models.DatasetBrief import DatasetBrief
 
-
 # --- Database Connection ---
 uri = os.getenv('MONGO_URI', 'mongodb://localhost:27017/')
 client = pymongo.MongoClient(uri, serverSelectionTimeoutMS=5000)
@@ -127,3 +126,13 @@ class DatasetRepository:
             dataset['lastModifiedBy']
         )
         return info
+
+    @staticmethod
+    def remove_dataset(dataset_id: str) -> None:
+        """
+        Удаляет датасет из БД.
+        """
+        print("--------", dataset_id)
+        db['DatasetInfoCollection'].delete_one(
+            {'_id': ObjectId(dataset_id)},
+        )
