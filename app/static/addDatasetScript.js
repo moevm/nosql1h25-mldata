140a6d666d6f8b1d7ce79f9f461c89d64255a1cf
Sergey Sokolov <sokolovserej@gmail.com>
2025-04-20 19:45:43 +0300

    [FIX] this event error

--- app/static/addDatasetScript.js
+++ app/static/addDatasetScript.js
@@ -1,21 +1,22 @@
 const FLASK_ROOT_URL = "http://127.0.0.1:5000"
-
+const MAIN_PAGE = FLASK_ROOT_URL + "/datasets";
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('dataset-form').addEventListener('submit', event => {
-        event.preventDefault();
+    const form = document.getElementById('dataset-form');
 
-        const formData = new FormData(this);
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        const formData = new FormData(form);
 
         fetch(FLASK_ROOT_URL + '/datasets/add', {
             method: 'POST',
-            body: formData
+            body: formData,
+            redirect: 'follow'
         })
             .then(response => {
                 if (!response.ok)
                     throw new Error('Response is not ok');
-
-                return response.json();
+                document.location.href = MAIN_PAGE;
             })
             .catch(error => {
                 console.error('Error:', error);
