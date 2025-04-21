2bcba979a1e09712c30dfc4ae0aadd280f6287ad
Sergey Sokolov <sokolovserej@gmail.com>
2025-04-21 07:58:59 +0300

    [UPD] change front add dataset

--- app/static/datasetScripts.js
+++ app/static/datasetScripts.js
@@ -1,29 +1,25 @@
 const FLASK_ROOT_URL = "http://127.0.0.1:5000"
 const MAIN_PAGE = FLASK_ROOT_URL + "/datasets";
 const DELETE_URL = MAIN_PAGE + "/delete/";
+const ADD_URL = MAIN_PAGE + "/add/";
 
-document.addEventListener('DOMContentLoaded', () => {
+function addDataset() {
     const form = document.getElementById('dataset-form');
+    const formData = new FormData(form);
 
-    form.addEventListener('submit', event => {
-        event.preventDefault();
-        const formData = new FormData(form);
-
-        fetch(FLASK_ROOT_URL + '/datasets/add', {
-            method: 'POST',
-            body: formData,
-            redirect: 'follow'
-        })
-            .then(response => {
-                if (!response.ok)
-                    throw new Error('Response is not ok');
-                document.location.href = MAIN_PAGE;
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-    });
-});
+    fetch(ADD_URL, {
+        method: 'POST',
+        body: formData,
+    }).then(response => {
+        if (!response.ok)
+            throw new Error('Response is not ok');
+        document.location.href = MAIN_PAGE;
+        console.log(document.location.href);
+    })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
 
 
 function deleteDataset(datasetId) {
