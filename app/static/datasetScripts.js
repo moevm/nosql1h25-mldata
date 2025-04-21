d9cfbd9ce14411c7f34161b41f224c7d10453a5c
Sergey Sokolov <sokolovserej@gmail.com>
2025-04-21 07:59:49 +0300

    [UPD] front edit dataset

--- app/static/datasetScripts.js
+++ app/static/datasetScripts.js
@@ -2,6 +2,7 @@ const FLASK_ROOT_URL = "http://127.0.0.1:5000"
 const MAIN_PAGE = FLASK_ROOT_URL + "/datasets";
 const DELETE_URL = MAIN_PAGE + "/delete/";
 const ADD_URL = MAIN_PAGE + "/add/";
+const EDIT_URL = MAIN_PAGE + "/edit/";
 
 function addDataset() {
     const form = document.getElementById('dataset-form');
@@ -21,6 +22,23 @@ function addDataset() {
         });
 }
 
+function editDataset(datasetId) {
+    const form = document.getElementById('dataset-form');
+    const formData = new FormData(form);
+
+    fetch(EDIT_URL + datasetId, {
+        method: 'PATCH',
+        body: formData,
+    })
+        .then(response => {
+            if (!response.ok)
+                throw new Error('Response is not ok');
+            document.location.href = MAIN_PAGE;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
 
 function deleteDataset(datasetId) {
     fetch(DELETE_URL + datasetId, {
