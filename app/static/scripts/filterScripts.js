d08e11e2cae455ba770259633cacb899a0cb75bf
Kirill Karpunin <kirill.karpunin20014@gmail.com>
2025-04-22 16:07:48 +0300

    [ADD] Add logic to draw filtered cards

--- app/static/scripts/filterScripts.js
+++ app/static/scripts/filterScripts.js
@@ -14,10 +14,52 @@ filterForm.addEventListener('submit', (e) => {
     }).then(response => {
         if (!response.ok)
             throw new Error('Response is not ok');
-        return response.text();
-    }).then(text => {
-        alert(text)
+        return response.json();
+    }).then(json => {
+        redrawCards(json);
     }).catch(error => {
         console.error('Error:', error);
     });
 })
+
+const container = document.getElementById('cards-container');
+
+function redrawCards(arrayOfBriefs) {
+    container.replaceChildren();
+
+    for (let brief of arrayOfBriefs) {
+        container.appendChild(createCard(brief));
+    }
+}
+
+function createCard(brief) {
+    const card = document.createElement('a');
+    card.className = 'ui card';
+    card.href = `/dataset/${brief.dataset_id}/`;
+
+    const content = document.createElement('div');
+    content.className = 'content';
+
+    const header = document.createElement('div');
+    header.className = 'header';
+    header.textContent = brief.dataset_name;
+
+    const description = document.createElement('div');
+    description.className = 'description';
+    description.textContent = brief.dataset_description;
+
+    const lineBreak = document.createElement('br');
+
+    const typeAndSize = document.createElement('div');
+    typeAndSize.className = 'description';
+    typeAndSize.textContent = `${brief.dataset_type}, ${brief.dataset_size} кбайт`;
+
+    content.appendChild(header);
+    content.appendChild(description);
+    content.appendChild(lineBreak);
+    content.appendChild(typeAndSize);
+
+    card.appendChild(content);
+
+    return card
+}
